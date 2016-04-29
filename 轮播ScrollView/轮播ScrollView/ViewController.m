44f9409470767ed53e850dc3c1554269525320f3
lujianwen <lujianwen_ios@sina.com>
2016-04-30 00:33:28 +0800

    test

--- 轮播ScrollView/轮播ScrollView/ViewController.m
+++ 轮播ScrollView/轮播ScrollView/ViewController.m
@@ -26,7 +26,6 @@
     self.upDownScrollView.contentSize = CGSizeMake(0, 800);
     [self.view addSubview:self.upDownScrollView];
     
-    //测试时实验数据，所以使用的是本地图片，正常应该是image_url
     NSMutableArray *datas = [NSMutableArray array];
     for (int i = 1; i <= 3; i++) {
         [datas addObject:[NSString stringWithFormat:@"%d",i]];
@@ -36,15 +35,12 @@
     self.scrollView.delegate = self;
     self.scrollView.allDatas = datas;
     [self.upDownScrollView addSubview:self.scrollView];
-    
 }
 
 - (void)didSelectedCircleScrollViewAtIndex:(NSInteger)index {
 
     UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"提示" message:[NSString stringWithFormat:@"点击了第%ld张",index] preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *alterAction = [UIAlertAction actionWithTitle:@"知道了" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
-        
-    }];
+    UIAlertAction *alterAction = [UIAlertAction actionWithTitle:@"知道了" style:UIAlertActionStyleCancel handler:nil];
     [controller addAction:alterAction];
     [self presentViewController:controller animated:YES completion:nil];
 }
